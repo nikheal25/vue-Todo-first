ac99d08d8ebe67be7285ae9bbc540d756fa6cd3c
Nick <nikheal25@gmail.com>
2020-12-21 08:24:49 +1100

    Showing todo list

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+  <div>
+    <Todos v-bind:todoList="todoList" /></div
+></template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Todos from "./components/Todos";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Todos,
+  },
+  data() {
+    return {
+      todoList: [
+        {
+          id: 1,
+          title: "Do homework",
+          completed: false,
+        },
+        {
+          id: 2,
+          title: "Do excercise",
+          completed: false,
+        },
+        {
+          id: 3,
+          title: "Cook lunch",
+          completed: false,
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-#app {
+* {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Todo List</h1>
+
+    <div v-for="todo in todoList" v-bind:key="todo.id">
+      <h3>{{ todo.title }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todos",
+  props: ["todoList"],
+};
+</script>
+
+<style scoped></style>
