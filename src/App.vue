6ab3a984c1a2538e0a9a9272ec20a7e59097b055
Nick <nikheal25@gmail.com>
2020-12-26 10:05:18 +1100

    added new folder

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,41 @@
 <template>
-  <div>
-    <Header /><AddTodo v-on:add-todo="addTodo" />
-    <Todos v-bind:todoList="todoList" v-on:del-todo="deleteTodo" /></div
-></template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <Header />
+  <router-view />
+</template>
 
 <script>
-import Todos from "./components/Todos";
 import Header from "./components/layout/Header";
-import AddTodo from "./components/AddTodo";
-import axios from "axios";
-
 export default {
   name: "App",
   components: {
-    Todos,
     Header,
-    AddTodo,
-  },
-  data() {
-    return {
-      todoList: [],
-    };
-  },
-  methods: {
-    deleteTodo(id) {
-      console.log(id);
-      //this.todoList = this.todoList.filter((todoItem) => todoItem.id !== id);
-      axios
-        .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-        .then(
-          (res) =>
-            (this.todoList = this.todoList.filter(
-              (todoItem) => todoItem.id !== id
-            ))
-        )
-        .catch((err) => console.log(err));
-    },
-    addTodo(newTodo) {
-      console.log("addind");
-
-      const { title, completed } = newTodo;
-      axios
-        .post("https://jsonplaceholder.typicode.com/todos", {
-          title,
-          completed,
-        })
-        .then((res) => (this.todoList = [...this.todoList, res.data]))
-        .catch((err) => console.log(err));
-
-      //this.todoList = [...this.todoList, newTodo];
-      console.log(this.todoList);
-    },
-  },
-
-  created() {
-    console.log("Created call");
-    axios
-      .get("https://jsonplaceholder.typicode.com/todos?_limit=5")
-      .then((res) => (this.todoList = res.data))
-      .catch((err) => console.log(err));
   },
 };
 </script>
 
 <style>
-* {
+#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos v-bind:todoList="todoList" v-on:del-todo="deleteTodo" /></div
+></template>
+
+<script>
+import Todos from "../components/Todos";
+import AddTodo from "../components/AddTodo";
+import axios from "axios";
+
+export default {
+  name: "Home",
+  components: {
+    Todos,
+    AddTodo,
+  },
+  data() {
+    return {
+      todoList: [],
+    };
+  },
+  methods: {
+    deleteTodo(id) {
+      console.log(id);
+      //this.todoList = this.todoList.filter((todoItem) => todoItem.id !== id);
+      axios
+        .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(
+          (res) =>
+            (this.todoList = this.todoList.filter(
+              (todoItem) => todoItem.id !== id
+            ))
+        )
+        .catch((err) => console.log(err));
+    },
+    addTodo(newTodo) {
+      console.log("addind");
+
+      const { title, completed } = newTodo;
+      axios
+        .post("https://jsonplaceholder.typicode.com/todos", {
+          title,
+          completed,
+        })
+        .then((res) => (this.todoList = [...this.todoList, res.data]))
+        .catch((err) => console.log(err));
+
+      //this.todoList = [...this.todoList, newTodo];
+      console.log(this.todoList);
+    },
+  },
+
+  created() {
+    console.log("Created call");
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=5")
+      .then((res) => (this.todoList = res.data))
+      .catch((err) => console.log(err));
+  },
+};
+</script>
+
+<style>
+* {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
